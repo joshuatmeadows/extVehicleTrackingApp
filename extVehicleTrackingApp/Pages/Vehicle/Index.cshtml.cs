0221725f070828998a481ff77815718affe22146
Joshua Meadows <55264026+joshuatmeadows@users.noreply.github.com>
2024-11-07 15:05:07 -0500

    Backend API integration

--- extVehicleTrackingApp/Pages/Vehicle/Index.cshtml.cs
+++ extVehicleTrackingApp/Pages/Vehicle/Index.cshtml.cs
@@ -6,24 +6,41 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using extVehicleTrackingApp.Data;
+using System.Net.Http;
 
 namespace extVehicleTrackingApp.Pages.Vehicle
 {
     public class IndexModel : PageModel
     {
-        private readonly extVehicleTrackingApp.Data.ApplicationDbContext _context;
+        private readonly HttpClient _httpClient;
 
-        public IndexModel(extVehicleTrackingApp.Data.ApplicationDbContext context)
+        public IndexModel(HttpClient httpClient)
         {
-            _context = context;
+            _httpClient=httpClient;
         }
 
         public IList<ExtVehicle> ExtVehicle { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(string OrgId)
         {
-            ExtVehicle = await _context.ExtVehicle
-                .Include(e => e.Org).ToListAsync();
+            if(string.IsNullOrEmpty(OrgId))
+            {
+                return Page();
+            }
+
+            try
+            {
+                // make the endpoint string we want to hit
+                string requestUrl = $"https://localhost:7099/api/Vehicle/getallbyorg?OrgId={Uri.EscapeDataString(OrgId)}";
+
+                // Send the GET request to the API
+                ExtVehicle = await _httpClient.GetFromJsonAsync<IList<ExtVehicle>>(requestUrl) ?? new List<ExtVehicle>();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return Page();
         }
     }
 }
